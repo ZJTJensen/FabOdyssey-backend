a98e01cee55545b678a6c6b6c2bb8e2c636294be
ZJTJensen <ZJTJensen@yahoo.com>
2024-06-20 00:24:24 -0400

    feat: test add logging

--- src/main/java/com/fabAdventure/controllers/UserController.java
+++ src/main/java/com/fabAdventure/controllers/UserController.java
@@ -101,6 +101,7 @@ public class UserController {
 	@PostMapping("/user/create")
 	public void createAccount(@RequestBody UsersRequest message) {
 		try {
+			System.out.println(message); // Add this line
 			System.err.println(message.getLocation() + message.getOriginLocation());
 			this.userService.creteUser(message.getPhoneNumber(), message.getDeck(), message.getUser(), message.getOriginLocation());
 		} catch (Exception e) {
